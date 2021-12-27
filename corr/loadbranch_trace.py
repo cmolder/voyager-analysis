ef9ace56cf29725ef1ef81b5a369974016f3d66d
Carson Molder <28611108+cmolder@users.noreply.github.com>
2021-12-26 19:52:18 -0600

    Batching out writes for loadbranch to reduce compression cost

--- corr/loadbranch_trace.py
+++ corr/loadbranch_trace.py
@@ -1,6 +1,6 @@
 """Read ChampSim trace and load trace simultaneously,
 trying to match LLC loads to their locations in
-the ChampSim trace. 
+the ChampSim trace.
 
 Once we do this, track the <n_branches>
 prior branch PCs + taken decisions. You can either
@@ -38,8 +38,9 @@ def assert_comparison(csim_inst, load_uiid, load_pc):
     assert len(csim_inst.src_mem) == 1, f'{load_uiid} matches with an instruction with 0 / 2+ load addreses {csim_inst.src_mem} : {csim_inst}'
     assert hex(csim_inst.pc) == load_pc, f'{load_uiid} pcs do not match: CS pc={hex(csim_inst.pc)}, load PC={load_pc}'
 
-    
+
 MAX_BRANCHES_TRACKED = 10000
+WRITE_INTERVAL = 1000 # Write in batches to reduce compression cost.
 def match_traces(cf, lf, branch_hist=0, max_inst=None, verbose=False, write_f=None):
     # Both traces are sorted temporally. So, we traverse along the
     # load trace and, for each load, search in increasing order
@@ -47,7 +48,8 @@ def match_traces(cf, lf, branch_hist=0, max_inst=None, verbose=False, write_f=No
     max_seen_uiid = 0
     branch_data = {}
     branch_uiids = []
-    
+    out_buffer = ''
+
     # Count number of lines
     nlines = 0
     for line in lf:
@@ -57,18 +59,18 @@ def match_traces(cf, lf, branch_hist=0, max_inst=None, verbose=False, write_f=No
     #for line in tqdm(lf, total=nlines, unit='line', dynamic_ncols=True):
     start = time.time()
     for lnum, line in enumerate(lf):
-        
+
         if lnum > 0 and lnum % 10000 == 0:
             pct = lnum / nlines
             elapsed_time = time.time() - start
             left_time = (elapsed_time / lnum * nlines) - elapsed_time # estimated time left
             print(f'{lnum} / {nlines} ({pct*100:.2f}%) ({elapsed_time / 60:.2f} min) ({left_time / 60:.2f} min est. rem.)')
-        
+
         # Parse load trace line
         if line.startswith('***') or line.startswith('Read'): # For handling some invalid lines in the ML-DPC load traces
             continue
         uiid, _, src_addr, pc, _ = parse_load_line(line)
-        
+
         # Return early if we exceed the maximum instruction limit
         if max_inst and uiid > max_inst:
             return
@@ -85,35 +87,35 @@ def match_traces(cf, lf, branch_hist=0, max_inst=None, verbose=False, write_f=No
                 if inst.is_branch: # Insert the branch into the sorted list (sorted by uiid)
                     bisect.insort(branch_uiids, i)
                     branch_data[i] = (inst.pc, inst.branch_taken)
-                    
+
                     # Clear old branch instructions to conserve memory
                     # (should not cause major impacts)
                     if len(branch_uiids) > MAX_BRANCHES_TRACKED:
                         remove_uiid = branch_uiids[0]
                         branch_uiids = branch_uiids[1:]
                         del branch_data[remove_uiid]
-                    
+
             max_seen_uiid = uiid
         #else:
         #    cf.seek((uiid - 1) * INST_SIZE)
         #    inst = next(get_instructions(cf), None)
-        
+
         # Assertion checks
         #assert_comparison(inst, uiid, pc)
-        
+
         if verbose:
             print(f'\n{uiid:8} Load    : pc={pc} src_mem={src_addr}')
             print(f'{uiid:8} CS      : pc={hex(inst.pc)} src_mem={hex(inst.src_mem[0])}')
             #print(f'{uiid:8} Load    : pc={bin(int(pc[2:], 16))} src_mem={bin(int(src_addr[2:], 16))}')
             #print(f'{uiid:8} CS      : pc={bin(inst.pc)} src_mem={bin(inst.src_mem[0])}')
-            
+
         # Match each load to its most recent branches, and print / write results.
         if branch_hist > 0:
             idx = bisect.bisect_left(branch_uiids, uiid)
             low, high = max(0, idx - branch_hist), min(idx, len(branch_uiids))
             prior_branch_uiids = branch_uiids[low:high][::-1] # Reverse so the most recent branch appears first.
-            
-            if verbose: 
+
+            if verbose:
                 for buiid in prior_branch_uiids:
                     pc, dec = branch_data[buiid]
                     print(f'({hex(pc)}, {"T" if dec else "NT"}) ', end='')
@@ -125,12 +127,17 @@ def match_traces(cf, lf, branch_hist=0, max_inst=None, verbose=False, write_f=No
             # Branch <n_branches> is the least recent branch.
             # If there are less than <n_branches> prior branches, the PC/Taken will both be 0.
             if write_f:
-                outline = line.rstrip('\n')
+                out_buffer += line.rstrip('\n')
                 for i in range(branch_hist):
                     pc, dec = branch_data[prior_branch_uiids[i]] if i < len(prior_branch_uiids) else (0, 0)
-                    outline += f', {hex(pc)[2:]}, {int(dec)}'
-                print(outline, file=write_f)
-            
+                    out_buffer += f', {hex(pc)[2:]}, {int(dec)}'
+                out_buffer += '\n'
+
+                # Batch out output writing
+                if lnum % WRITE_INTERVAL == 0:
+                    print(out_buffer, end='', file=write_f)
+                    out_buffer = ''
+
 
 
 """Driver (and helper) functions"""
@@ -165,24 +172,24 @@ def get_open_function(path):
 
 if __name__ == '__main__':
     args = get_arguments()
-    
+
     cs_open = get_open_function(args.champsim_trace)
     l_open = get_open_function(args.load_trace)
     o_open = get_open_function(args.output_trace) if args.output_trace else None
-        
+
     if args.output_trace:
         of = o_open(args.output_trace, mode='wt', encoding='utf-8')
     else: of = None
-    
+
     with cs_open(args.champsim_trace, mode='rb') as cf, l_open(args.load_trace, mode='rt', encoding='utf-8') as lf:
         match_traces(
-            cf, lf, 
+            cf, lf,
             branch_hist=args.n_branches,
             max_inst=args.max_inst,
             verbose=args.verbose,
             write_f=of
         )
-    
+
     if args.output_trace:
         of.close()
 
---
+++ corr/match_traces_branches.py
@@ -0,0 +1,115 @@
+"""Read ChampSim trace and load trace simultaneously,
+trying to match LLC loads to their locations in
+the ChampSim trace. If something is off, an
+error is raised explaining the issue.
+
+Additionally, we will report the M most recent branches (PCs and their
+decisions).
+
+Need to run from above corr/ directory. If you still get an error,
+try export PYTHONPATH=.
+"""
+
+import lzma
+import gzip
+import argparse
+import bisect
+from utils.champsim_trace import get_instructions, INST_SIZE
+
+
+def parse_load_line(line):
+    uiid, cycle, src_addr, pc, cache_hit = tuple(s.replace(' ', '') for s in line.split(','))
+    uiid = int(uiid)
+    cycle = int(cycle)
+    src_addr = '0x' + src_addr
+    pc = '0x' + pc
+    cache_hit = bool(cache_hit)
+    return uiid, cycle, src_addr, pc, cache_hit
+
+
+def match_traces(cf, lf, branch_hist=0):
+    # Both traces are sorted temporally. So, we traverse along the
+    # load trace and, for each load, search in increasing order
+    # for the matching load in the ChampSim trace.
+    prev_uiid = 0
+    src_mapping = {}
+    branches = []
+
+    for line in lf:
+        # For handling some invalid lines in the ML-DPC load traces
+        if line.startswith('***') or line.startswith('Read'):
+            continue
+        uiid, _, src_addr, pc, _ = parse_load_line(line)
+
+        if uiid > prev_uiid:
+            for i in range(prev_uiid, uiid):
+                inst = next(get_instructions(cf), None)
+                if inst.is_branch: # Insert the branch into the sorted list (sorted by uiid)
+                    bisect.insort(branches, (i, inst.pc, inst.branch_taken))
+        else:
+            cf.seek((uiid - 1) * INST_SIZE)
+            inst = next(get_instructions(cf), None)
+        prev_uiid = uiid
+
+        if not inst:
+            print('Done matching traces. Everything checks out.')
+            return
+
+        # Assertion checks
+        assert len(inst.src_mem) == 1, f'{uiid} matches with instruction with 0 / 2+ load addreses {inst.src_mem} : {inst}'
+        assert hex(inst.pc) == pc, f'{uiid} pcs do not match: CS pc={hex(inst.pc)}, load PC={pc}'
+        if src_addr in src_mapping:
+            assert src_mapping[src_addr] == inst.src_mem, '{uiid} mapping between addresses is not one-to-one: Load {src_addr}, CS {hex(inst.pc)} vs. {hex(src_mapping[src_addr])}'
+        src_mapping[src_addr] = inst.src_mem
+
+        print()
+        print(f'{uiid:8} Load    : pc={pc} src_mem={src_addr}')
+        print(f'{uiid:8} CS      : pc={hex(inst.pc)} src_mem={hex(inst.src_mem[0])}')
+        #print(f'{uiid:8} Load    : pc={bin(int(pc[2:], 16))} src_mem={bin(int(src_addr[2:], 16))}')
+        #print(f'{uiid:8} CS      : pc={bin(inst.pc)} src_mem={bin(inst.src_mem[0])}')
+        if branch_hist > 0:
+            i = bisect.bisect_left(branches, (uiid, 0, 0))
+            low = max(0, i - branch_hist)
+            high = min(i, len(branches))
+            prior_branches = branches[low:high]
+            print(f'{uiid:8} Branches: ', end='')
+            for _, pc, dec in prior_branches:
+                print(f'({hex(pc)}, {"T" if dec else "NT"}) ', end='')
+            print()
+
+
+"""Driver (and helper) functions"""
+def get_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('champsim_trace')
+    parser.add_argument('load_trace')
+    parser.add_argument('n_branches', type=int)
+    args = parser.parse_args()
+
+    print('Arguments:')
+    print('    ChampSim trace :', args.champsim_trace)
+    print('    Load trace     :', args.load_trace)
+    print('    Num branches   :', args.n_branches)
+
+    return args
+
+
+if __name__ == '__main__':
+    args = get_argument_parser()
+    if args.champsim_trace.endswith('xz'):
+        cs_open = lzma.open
+    elif args.champsim_trace.endswith('gz'):
+        cs_open = gzip.open
+    else:
+        cs_open = open
+
+    if args.load_trace.endswith('xz'):
+        l_open = lzma.open
+    elif args.load_trace.endswith('gz'):
+        l_open = gzip.open
+    else:
+        l_open = open
+
+    with cs_open(args.champsim_trace, mode='rb') as cf, l_open(args.load_trace, mode='rt', encoding='utf-8') as lf:
+        match_traces(cf, lf, branch_hist=args.n_branches)
+
